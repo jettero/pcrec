cd300ef588e1977cf036c626eb542a585f40dfa3
Paul Miller <paul@jettero.pl>
2023-08-26 12:55:44 -0400

    this way I can actiavte trace when I want it (only)

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,20 @@ import (
 	"github.com/jettero/pcrec/lib"
 	"github.com/k0kubun/pp/v3"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func truthy(s string) bool {
+	s = strings.ToLower(s)
+	if s == "true" || s == "yes" {
+		return true
+	}
+	if num, err := strconv.Atoi(s); err == nil {
+		return num != 0
+	}
+	return false
+}
 
 func main() {
 	fmt.Printf("ENBF:\n%s\n\n", lib.Parser)
@@ -16,12 +28,16 @@ func main() {
 		fmt.Printf("-------------=: parsing, \"%s\"\n", arg)
 		var trace strings.Builder
 		ast, err := lib.Parser.ParseString("-", arg, participle.Trace(&trace))
-		if err != nil {
+		if truthy(os.Getenv("PCREC_TRACE")) {
 			fmt.Printf("TRACE:\n%s", trace.String())
+		}
+		if err != nil {
+			if truthy(os.Getenv("PCREC_TRACE_ERROR")) {
+				fmt.Printf("TRACE:\n%s", trace.String())
+			}
 			fmt.Printf("RE-ARG: %s\n", arg)
 			fmt.Printf("ERROR:  %+v\n", err)
 		} else {
-			// fmt.Printf("TRACE:\n%s", trace.String())
 			pp.Print(ast) // why doesn't this print its own newlin? pfft
 			fmt.Println()
 		}
