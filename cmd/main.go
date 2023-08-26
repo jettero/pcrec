27745eecc36045600110f5e522dd3f82496bc716
Paul Miller <paul@jettero.pl>
2023-08-26 12:16:55 -0400

    enbf doesn't really get us anywhere, but the trace does (I guess)

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/alecthomas/participle/v2"
 	"github.com/jettero/pcrec/lib"
 	"github.com/k0kubun/pp/v3"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,10 +14,14 @@ func main() {
 
 	for _, arg := range os.Args[1:] {
 		fmt.Printf("-------------=: parsing, \"%s\"\n", arg)
-		ast, err := lib.Parser.ParseString("-", arg)
+		var trace strings.Builder
+		ast, err := lib.Parser.ParseString("-", arg, participle.Trace(&trace))
 		if err != nil {
-			fmt.Printf("ERROR: %+v\n", err)
+			fmt.Printf("TRACE:\n%s", trace.String())
+			fmt.Printf("RE-ARG: %s\n", arg)
+			fmt.Printf("ERROR:  %+v\n", err)
 		} else {
+			// fmt.Printf("TRACE:\n%s", trace.String())
 			pp.Print(ast) // why doesn't this print its own newlin? pfft
 			fmt.Println()
 		}
