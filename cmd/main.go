4c231969cd457b8b0047c80a2ca11372e4754669
Paul Miller <paul@jettero.pl>
2023-09-24 13:03:55 -0400

    add some args processing and use the new pretty printer

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/jettero/pcrec"
+	"github.com/jettero/pcrec/lib"
 	"github.com/k0kubun/pp/v3"
+	"github.com/spf13/pflag"
 	"os"
 )
 
+func ProcessArgs() []string {
+	var trace *bool = pflag.BoolP("verbose", "v", false, "print verbose debug messages during the RE parse")
+	var ppp *bool = pflag.BoolP("pp", "p", false, "print the analysis with k0kubun/pp rather than the internal formatter")
+	var halp *bool = pflag.BoolP("help", "h", false, "show the help screen text")
+
+	pflag.Parse()
+
+	if *halp {
+		b := bytes.NewBufferString("\nUSAGE: pcrec [--options] [pattern [pattern …]]\n")
+		pflag.CommandLine.SetOutput(b)
+		pflag.PrintDefaults()
+		fmt.Println(b.String())
+		os.Exit(0)
+	}
+
+	if *trace {
+		os.Setenv("PCREC_TRACE", "yes")
+	}
+
+	if *ppp {
+		os.Setenv("PCREC_PP_NFA", "yes")
+	}
+
+	return pflag.Args()
+}
+
 func main() {
-	for _, arg := range os.Args[1:] {
+	for _, arg := range ProcessArgs() {
 		nfa, err := pcrec.ParseString(arg)
-		pp.Print(nfa) // why doesn't this print its own newlin? pfft
-		fmt.Println()
+
+		fmt.Printf("\nNFA Description for \"%s\":\n", arg)
+		if lib.TruthyEnv("PCREC_PP_NFA") {
+			pp.Println(nfa)
+		} else {
+			fmt.Print(nfa.Describe(1))
+		}
+
 		if err != nil {
 			fmt.Printf("RE-ARG: %s\n", arg)
 			fmt.Printf("%+v\n", err)
