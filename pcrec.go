6a64405ce387012dbb86bbc4d92fcb537b900392
Paul Miller <paul@jettero.pl>
2023-10-01 23:42:46 -0400

    hilarious bug

--- pcrec.go
+++ pcrec.go
@@ -7,7 +7,7 @@ func Parse(pat string) (*lib.NFA, error) {
 }
 
 func Search(pat string, candidate string) (*lib.REsult, error) {
-	if n, err := Parse(candidate); err != nil {
+	if n, err := Parse(pat); err != nil {
 		return nil, err
 	} else {
 		return n.SearchRunes([]rune(candidate)), nil
