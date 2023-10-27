b87c0ac9d37e3ad3d0233ff8b5e5acb9a3348d2e
Paul Miller <paul@jettero.pl>
2023-10-26 22:17:42 -0400

    old and shitty

--- two-desc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for item in "$@"; do
-    echo;echo "----=: $item :=---=: $(date) :=----"
-    go run ./cmd/expr/main.go "$item"
-    echo
-    go run ./cmd/expr/main.go -D "$item" | tee pat.dot
-done
