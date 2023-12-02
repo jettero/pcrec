f4b3f9f4348c16c08343a96fd7d1fea37af1ce67
Paul Miller <paul@jettero.pl>
2023-12-02 11:11:45 -0500

    more notes and thoughts on this new struct setup

--- lib/graph.go
+++ lib/graph.go
@@ -1,19 +1,32 @@
 package lib
 
 type State struct {
-	// Mostly just placeholders for connecting edges?
-	// Surely we here track our descent though, right?
-	//     Yeah, our counters
-	//     and quantities go here for sure
+	Xfer   map[*Transition]*State // *State is nil for Accept?
+	Accept bool                   // or sould we have an explicit accepting state?
+	Qty    *Qty                   // XXX do states have quantities? are they shared with Transitions?
+}
+
+type Qty struct {
+	Min int
+	Max int // -1 indicates ∞
+
+	// Is this quantity capturing?
+	Capture int // 0, or the number of the capture group
 }
 
 type Transition struct {
-	Inverse bool
-	First   rune
-	Last    rune
-	// What do we consume?
-	// Do we capture it?
-	// Perhaps we here record what positions in the string we've eaten?
-	//     How do we reverse it when we backtrack?
-	//     Is this a pushdown stack of captures?
+	// What we consume in the transition
+	First rune // stated as a range
+	Last  rune // of contiguous runes
+
+	// although, perhaps we say
+	Inverse bool // anything except the above
+
+	// and we can only use this transition if the
+	Qty *Qty // XXX does nil mean anything?
+	// XXX This is a ptr so we can share the Qty between things
+	// XXX Maybe multiple Transitions?
+	// XXX Or what do States store?
+	// XXX should this even be here? should it be in State?
+	// XXX prolly, if *Qty is nil, we mean exactly 1? or do we always populate this?
 }
